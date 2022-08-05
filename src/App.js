1498aa0d2c1b3672be973307343552c5d1d798a3
Mazdul1000 <mazdul1000@gmail.com>
2022-08-05 18:39:02 +0600

    createing the authentication routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Dashboard from './Dashboard/Dashboard';
+import LoginPage from './authPages/LoginPage/LoginPage';
+import RegisterPage from './authPages/RegisterPage/RegisterPage';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <Routes>
+        <Route path='/' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/login' element={<LoginPage></LoginPage>}></Route>
+        <Route path='/register' element={<RegisterPage></RegisterPage>}></Route>
+      </Routes>
     </div>
   );
 }
